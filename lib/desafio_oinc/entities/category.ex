c7282e82b613ce8dbefc56ca234d2d4fcc8f6783
brafa <bruno.rafael.vicente@hotmail.com>
2023-12-01 13:30:39 -0300

    add entities (#5)
    * add entities
    
    * fix credo

---
+++ lib/desafio_oinc/entities/category.ex
@@ -0,0 +1,23 @@
+defmodule DesafioOinc.Entities.Category do
+  @moduledoc false
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "categories" do
+    field :name, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(attrs) do
+    changeset(%__MODULE__{}, attrs)
+  end
+
+  @doc false
+  def changeset(category, attrs) do
+    category
+    |> cast(attrs, [:name])
+    |> validate_required([])
+  end
+end
---
+++ lib/desafio_oinc/entities/user.ex
@@ -0,0 +1,29 @@
+defmodule DesafioOinc.Entities.User do
+  @moduledoc false
+  use Ecto.Schema
+
+  alias DesafioOinc.Entities.Post
+
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :bio, :string
+
+    has_many :posts, Post, foreign_key: :author_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(attrs) do
+    changeset(%__MODULE__{}, attrs)
+  end
+
+  @doc false
+  def changeset(%__MODULE__{} = user, attrs) do
+    user
+    |> cast(attrs, [:name, :bio])
+    |> validate_required([])
+  end
+end
